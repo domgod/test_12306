73cf032d5e24d3659819fd3be2eaafff151a9531
domgod <2651690283@qq.com>
2024-01-22 22:52:12 +0800

    添加项目文件。

---
+++ test_2306/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace test_2306
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ZhangHaoMing = new System.Windows.Forms.TextBox();
+            this.ZhangHao = new System.Windows.Forms.Label();
+            this.MiMa = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.DengLu = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker_ChuFaShiJian = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.textBox_MuDiDi = new System.Windows.Forms.TextBox();
+            this.textBox_ChuFaDi = new System.Windows.Forms.TextBox();
+            this.ChaXun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ZhangHaoMing
+            // 
+            this.ZhangHaoMing.Location = new System.Drawing.Point(59, 10);
+            this.ZhangHaoMing.Name = "ZhangHaoMing";
+            this.ZhangHaoMing.Size = new System.Drawing.Size(206, 21);
+            this.ZhangHaoMing.TabIndex = 0;
+            // 
+            // ZhangHao
+            // 
+            this.ZhangHao.AutoSize = true;
+            this.ZhangHao.Location = new System.Drawing.Point(12, 13);
+            this.ZhangHao.Name = "ZhangHao";
+            this.ZhangHao.Size = new System.Drawing.Size(53, 12);
+            this.ZhangHao.TabIndex = 1;
+            this.ZhangHao.Text = "账号名：";
+            // 
+            // MiMa
+            // 
+            this.MiMa.AutoSize = true;
+            this.MiMa.Location = new System.Drawing.Point(301, 13);
+            this.MiMa.Name = "MiMa";
+            this.MiMa.Size = new System.Drawing.Size(41, 12);
+            this.MiMa.TabIndex = 2;
+            this.MiMa.Text = "密码：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(336, 10);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(206, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // DengLu
+            // 
+            this.DengLu.Location = new System.Drawing.Point(580, 12);
+            this.DengLu.Name = "DengLu";
+            this.DengLu.Size = new System.Drawing.Size(75, 23);
+            this.DengLu.TabIndex = 4;
+            this.DengLu.Text = "登录";
+            this.DengLu.UseVisualStyleBackColor = true;
+            this.DengLu.Click += new System.EventHandler(this.DengLu_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1, 202);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(799, 247);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "出发时间：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(200, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "返回时间：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(196, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "目标地点";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "起始地点";
+            // 
+            // dateTimePicker_ChuFaShiJian
+            // 
+            this.dateTimePicker_ChuFaShiJian.Location = new System.Drawing.Point(68, 48);
+            this.dateTimePicker_ChuFaShiJian.Name = "dateTimePicker_ChuFaShiJian";
+            this.dateTimePicker_ChuFaShiJian.Size = new System.Drawing.Size(113, 21);
+            this.dateTimePicker_ChuFaShiJian.TabIndex = 10;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(255, 48);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(110, 21);
+            this.dateTimePicker2.TabIndex = 11;
+            // 
+            // textBox_MuDiDi
+            // 
+            this.textBox_MuDiDi.Location = new System.Drawing.Point(255, 100);
+            this.textBox_MuDiDi.Name = "textBox_MuDiDi";
+            this.textBox_MuDiDi.Size = new System.Drawing.Size(110, 21);
+            this.textBox_MuDiDi.TabIndex = 12;
+            // 
+            // textBox_ChuFaDi
+            // 
+            this.textBox_ChuFaDi.Location = new System.Drawing.Point(68, 100);
+            this.textBox_ChuFaDi.Name = "textBox_ChuFaDi";
+            this.textBox_ChuFaDi.Size = new System.Drawing.Size(99, 21);
+            this.textBox_ChuFaDi.TabIndex = 13;
+            // 
+            // ChaXun
+            // 
+            this.ChaXun.Location = new System.Drawing.Point(388, 70);
+            this.ChaXun.Name = "ChaXun";
+            this.ChaXun.Size = new System.Drawing.Size(75, 23);
+            this.ChaXun.TabIndex = 14;
+            this.ChaXun.Text = "查询";
+            this.ChaXun.UseVisualStyleBackColor = true;
+            this.ChaXun.Click += new System.EventHandler(this.ChaXun_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ChaXun);
+            this.Controls.Add(this.textBox_ChuFaDi);
+            this.Controls.Add(this.textBox_MuDiDi);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker_ChuFaShiJian);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.DengLu);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.MiMa);
+            this.Controls.Add(this.ZhangHao);
+            this.Controls.Add(this.ZhangHaoMing);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ZhangHaoMing;
+        private System.Windows.Forms.Label ZhangHao;
+        private System.Windows.Forms.Label MiMa;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button DengLu;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_ChuFaShiJian;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.TextBox textBox_MuDiDi;
+        private System.Windows.Forms.TextBox textBox_ChuFaDi;
+        private System.Windows.Forms.Button ChaXun;
+    }
+}
+
---
+++ test_2306/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_2306
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void DengLu_Click(object sender, EventArgs e)
+        {
+            //方式1:简单方法
+            //string strHTML = "";
+            //WebClient myWebClient = new WebClient();
+            //Stream myStream = myWebClient.OpenRead("https://kyfw.12306.cn/otn/leftTicket/queryE?leftTicketDTO.train_date="+DateTime.Now.ToString("yyyy-MM-dd")+" &leftTicketDTO.from_station=SZQ&leftTicketDTO.to_station=HDP&purpose_codes=ADULT");
+            //StreamReader sr = new StreamReader(myStream, System.Text.Encoding.GetEncoding("utf-8"));
+            //strHTML = sr.ReadToEnd();
+            //myStream.Close();
+            //textBox3.Text= strHTML;
+
+            //方式二：中等方法
+            //string urll = "https://kyfw.12306.cn/otn/leftTicket/queryE?leftTicketDTO.train_date=2024-01-23&leftTicketDTO.from_station=SZQ&leftTicketDTO.to_station=HDP&purpose_codes=ADULT";
+            //Uri uri = new Uri(urll);
+            //WebRequest myReq = WebRequest.Create(urll);
+            //WebResponse result = myReq.GetResponse();
+            //Stream receviceStream = result.GetResponseStream();
+            //StreamReader readerOfStream = new StreamReader(receviceStream, System.Text.Encoding.GetEncoding("utf-8"));
+            //string strHTML = readerOfStream.ReadToEnd();
+            //readerOfStream.Close();
+            //receviceStream.Close();
+            //result.Close();
+            //textBox3.Text = strHTML;
+            //复杂方法：可以带cookie
+            /// <summary>
+            /// 获取页面html
+            /// </summary>
+            /// <param name="uri">访问url</param>
+            /// <param name="refererUri">来源url</param>
+            /// <param name="encodingName">编码名称 例如：gb2312</param>
+            /// <returns></returns>
+           
+               // string uri = "https://kyfw.12306.cn/";
+               
+                string encodingName = "utf-8";
+                string refererUri = "https://kyfw.12306.cn/otn/leftTicket/queryE?leftTicketDTO.train_date=2024-01-23&leftTicketDTO.from_station=SZQ&leftTicketDTO.to_station=HDP&purpose_codes=ADULT";
+                string uri = refererUri;
+
+                string html = string.Empty;
+                CookieContainer cookieContainer = new CookieContainer();
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+                cookieContainer.Add(new Cookie("BAIDUID", "67017F5C6A5EE8351192F7D34E7A221E:FG=1", "", "kyfw.12306.cn"));
+                cookieContainer.Add(new Cookie("PSTM", "1523879243", "", "kyfw.12306.cn"));
+                cookieContainer.Add(new Cookie("BIDUPSID", "A29EA919049CED566C183C7ED175C6AB", "", "kyfw.12306.cn"));
+                cookieContainer.Add(new Cookie("BD_UPN", "1a314353", "", "kyfw.12306.cn"));
+                cookieContainer.Add(new Cookie("BDUSS", "1F4Wk1EUUxEWkNEZS1lUWdSNkFWOW5IbThoYXNYcktMWmhmRkE5MkxvQU9Jd0piQVFBQUFBJCQAAAAAAAAAAAEAAAD9qTIYw867wzGw19K5AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA6W2loOltpaY", "", "kyfw.12306.cn"));
+                request.ContentType = "text/html;charset=" + encodingName;
+                request.Method = "Get";
+                request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36 Core/1.63.5221.400 QQBrowser/10.0.1125.400";
+                request.CookieContainer = cookieContainer;
+
+                if (!string.IsNullOrEmpty(refererUri))
+                    request.Referer = refererUri;
+
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    using (Stream streamResponse = response.GetResponseStream())
+                    {
+                        using (StreamReader streamResponseReader = new StreamReader(streamResponse, Encoding.GetEncoding(encodingName)))
+                        {
+                            html = streamResponseReader.ReadToEnd();
+                        }
+                    }
+                }
+
+            textBox1.Text = html.ToString();
+
+
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ChaXun_Click(object sender, EventArgs e)
+        {
+
+            /// <summary>
+            /// 获取页面html
+            /// </summary>
+            /// <param name="uri">访问url</param>
+            /// <param name="refererUri">来源url</param>
+            /// <param name="encodingName">编码名称 例如：gb2312</param>
+            /// <returns></returns>
+
+            // string uri = "https://kyfw.12306.cn/otn/leftTicket/queryE?leftTicketDTO.train_date=2024-01-24&leftTicketDTO.from_station=SZQ&leftTicketDTO.to_station=HDP&purpose_codes=ADULT";
+            string uri = "https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.9297";
+            string refererUri = uri;
+            string encodingName = "utf-8";
+
+            string html_DiZhiBianMa = string.Empty;
+            CookieContainer cookieContainer = new CookieContainer();
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+
+            cookieContainer.Add(new Cookie("_uab_collina", "170554335525961696602851", "/otn/leftTicket", "kyfw.12306.cn"));
+            cookieContainer.Add(new Cookie("JSESSIONID", "7635D7764CCF7C700C622EFF8E92552E", "/otn", "kyfw.12306.cn"));
+            cookieContainer.Add(new Cookie("BIGipServerpassport", "904397066.50215.0000", "/", "kyfw.12306.cn"));
+            cookieContainer.Add(new Cookie("guidesStatus", "off", "/", ".12306.cn"));
+            cookieContainer.Add(new Cookie("highContrastMode", "defaltMode", "/", ".12306.cn"));
+            cookieContainer.Add(new Cookie("route", "c5c62a339e7744272a54643b3be5bf64", "/", "kyfw.12306.cn"));
+            cookieContainer.Add(new Cookie("BIGipServerotn", "2564227338.24610.0000", "/", "kyfw.12306.cn"));
+            cookieContainer.Add(new Cookie("BIGipServerpassport", "904397066.50215.0000", "/", "kyfw.12306.cn"));
+
+
+            //cookieContainer.Add(new Cookie("PSTM", "1523879243", "", "www.baidu.com"));
+            //cookieContainer.Add(new Cookie("BIDUPSID", "A29EA919049CED566C183C7ED175C6AB", "", "www.baidu.com"));
+            //cookieContainer.Add(new Cookie("BD_UPN", "1a314353", "", "www.baidu.com"));
+            //cookieContainer.Add(new Cookie("BDUSS", "1F4Wk1EUUxEWkNEZS1lUWdSNkFWOW5IbThoYXNYcktMWmhmRkE5MkxvQU9Jd0piQVFBQUFBJCQAAAAAAAAAAAEAAAD9qTIYw867wzGw19K5AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA6W2loOltpaY", "", "www.baidu.com"));
+            request.ContentType = "text/html;charset=" + encodingName;
+            request.Method = "Get";
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36 Core/1.63.5221.400 QQBrowser/10.0.1125.400";
+            request.CookieContainer = cookieContainer;
+
+            if (!string.IsNullOrEmpty(refererUri))
+                request.Referer = refererUri;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                using (Stream streamResponse = response.GetResponseStream())
+                {
+                    using (StreamReader streamResponseReader = new StreamReader(streamResponse, Encoding.GetEncoding(encodingName)))
+                    {
+                        html_DiZhiBianMa = streamResponseReader.ReadToEnd();
+                    }
+                }
+            }
+            textBox1.Text = html_DiZhiBianMa;
+
+
+            string FormAddress = string.Empty;
+            string ToAddress = string.Empty;
+            int FormAddress_Length = textBox_ChuFaDi.TextLength;
+            int ToAddress_Length = textBox_MuDiDi.TextLength;
+            string BianMa1 = html_DiZhiBianMa;
+            while (true)
+            {
+                BianMa1 = BianMa1.Substring(BianMa1.IndexOf(textBox_ChuFaDi.Text + "|") + FormAddress_Length + 1);
+                if (Regex.IsMatch(BianMa1.Substring(0, 1), "[A-Z{3}]"))
+                {
+                    break;
+                }
+            }
+            FormAddress = BianMa1.Substring(0, 3);
+
+            string BianMa2 = html_DiZhiBianMa;
+            while (true)
+            {
+                BianMa2 = BianMa2.Substring(BianMa2.IndexOf(textBox_MuDiDi.Text + "|") + ToAddress_Length + 1);
+                if (Regex.IsMatch(BianMa2.Substring(0, 1), "[A-Z{3}]"))
+                {
+                    break;
+                }
+            }
+            ToAddress = BianMa2.Substring(0, 3);
+
+            uri = "https://kyfw.12306.cn/otn/leftTicket/queryE?leftTicketDTO.train_date=" + dateTimePicker_ChuFaShiJian.Value.ToString("yyyy-MM-dd") + "&leftTicketDTO.from_station=" + FormAddress + "&leftTicketDTO.to_station=" + ToAddress + "&purpose_codes=ADULT";
+            refererUri = uri;
+            string html = string.Empty;
+            HttpWebRequest request_html = (HttpWebRequest)WebRequest.Create(uri);
+            request_html.ContentType = "text/html;charset=" + encodingName;
+            request_html.Method = "Get";
+            request_html.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.26 Safari/537.36 Core/1.63.5221.400 QQBrowser/10.0.1125.400";
+            request_html.CookieContainer = cookieContainer;
+
+            if (!string.IsNullOrEmpty(refererUri))
+                request_html.Referer = refererUri;
+
+            using (HttpWebResponse response = (HttpWebResponse)request_html.GetResponse())
+            {
+                using (Stream streamResponse = response.GetResponseStream())
+                {
+                    using (StreamReader streamResponseReader = new StreamReader(streamResponse, Encoding.GetEncoding(encodingName)))
+                    {
+                        html = streamResponseReader.ReadToEnd();
+                    }
+                }
+            }
+            textBox1.Text = html;
+
+
+        }
+    }
+}
